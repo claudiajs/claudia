116a5bd6f23d47287f4f1a01b93e17dd485aeb29
Gojko Adzic <gojko@gojko.com>
2016-02-05 01:08:29 +0100

    loadconfig spec

---
+++ spec/loadconfig-spec.js
@@ -0,0 +1,68 @@
+/*global describe, it, expect, beforeEach, afterEach, require */
+var tmppath = require('../src/util/tmppath'),
+	shell = require('shelljs'),
+	fs = require('fs'),
+	underTest = require('../src/tasks/loadconfig');
+describe('loadconfig', function () {
+	'use strict';
+	var workingdir;
+	beforeEach(function () {
+		workingdir = tmppath();
+		shell.mkdir(workingdir);
+		shell.cd(workingdir);
+	});
+	afterEach(function () {
+		shell.rm('-rf', workingdir);
+	});
+	it('fails if package.json is missing', function (done) {
+		underTest().then(done.fail, function (message) {
+			expect(message).toEqual('package.json is missing');
+			done();
+		});
+		it('fails if package.json is not valid json', function (done) {
+			fs.writeFileSync('package.json', 'not-json', 'utf8');
+			underTest().then(done.fail, function (message) {
+				expect(message).toEqual('invalid configuration in package.json');
+				done();
+			});
+		});
+	});
+	describe('when package.json is present', function () {
+		beforeEach(function () {
+			fs.writeFileSync('package.json', JSON.stringify({pack: 'me'}), 'utf8');
+		});
+		it('fails if beamup.json is not in the current working dir', function (done) {
+			underTest().then(done.fail, function (message) {
+				expect(message).toEqual('beamup.json is missing');
+				done();
+			});
+		});
+		it('fails if beamup.json is not valid json', function (done) {
+			fs.writeFileSync('beamup.json', 'not-json', 'utf8');
+			underTest().then(done.fail, function (message) {
+				expect(message).toEqual('invalid configuration in beamup.json');
+				done();
+			});
+		});
+
+	});
+	describe('when both package.json and beamup.json are present', function () {
+		beforeEach(function () {
+			fs.writeFileSync('beamup.json', JSON.stringify({a: 'b'}), 'utf8');
+			fs.writeFileSync('package.json', JSON.stringify({pack: 'me'}), 'utf8');
+		});
+		it('returns the contents of beamup.json as config', function (done) {
+			underTest().then(function (result) {
+				expect(result.config).toEqual({a: 'b'});
+				done();
+			}, done.fail);
+		});
+		it('returns the contents of package.json as package', function (done) {
+			underTest().then(function (result) {
+				expect(result.package).toEqual({pack: 'me'});
+				done();
+			}, done.fail);
+		});
+	});
+
+});
