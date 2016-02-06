8243d93ced1e89e18f17af40e7c32d758c67fdc9
Gojko Adzic <gojko@gojko.com>
2016-02-06 22:04:19 +0100

    update command

---
+++ spec/update-spec.js
@@ -0,0 +1,82 @@
+/*global describe, require, it, expect, beforeEach, afterEach, console, jasmine */
+var underTest = require('../src/commands/update'),
+	create = require('../src/commands/create'),
+	shell = require('shelljs'),
+	tmppath = require('../src/util/tmppath'),
+	fs = require('fs'),
+	path = require('path'),
+	aws = require('aws-sdk'),
+	Promise = require('bluebird'),
+	awsRegion = 'us-east-1';
+describe('update', function () {
+	'use strict';
+	var workingdir, cwd, testRunName, iam, lambda, newObjects, originalTimeout;
+	beforeEach(function () {
+		workingdir = tmppath();
+		cwd = shell.pwd();
+		testRunName = 'test' + Date.now();
+		iam = new aws.IAM();
+		lambda = new aws.Lambda({region: awsRegion});
+		newObjects = false;
+		originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
+		shell.mkdir(workingdir);
+	});
+	afterEach(function (done) {
+		var deleteRole = Promise.promisify(iam.deleteRole.bind(iam)),
+			deleteFunction = Promise.promisify(lambda.deleteFunction.bind(lambda));
+		shell.cd(cwd);
+		jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+		if (shell.test('-e', workingdir)) {
+			shell.rm('-rf', workingdir);
+		}
+		if (!newObjects || !newObjects.lambdaFunction) {
+			return done();
+		}
+		deleteFunction({FunctionName: newObjects.lambdaFunction}).then(function () {
+			if (!newObjects || !newObjects.lambdaRole) {
+				return Promise.resolve();
+			}
+			return deleteRole({RoleName: newObjects.lambdaRole});
+		}).catch(function (err) {
+			console.log('error cleaning up', err);
+		}).finally(done);
+	});
+	it('fails when the source dir does not contain the project config file', function (done) {
+		underTest({source: workingdir}).then(done.fail, function (reason) {
+			expect(reason).toEqual('claudia.json does not exist in the source folder');
+			done();
+		});
+	});
+	it('fails when the project config file does not contain the lambda name', function (done) {
+		fs.writeFileSync(path.join(workingdir, 'claudia.json'), '{}', 'utf8');
+		underTest({source: workingdir}).then(done.fail, function (reason) {
+			expect(reason).toEqual('invalid configuration -- lambda.name missing from claudia.json');
+			done();
+		});
+	});
+	it('fails when the project config file does not contain the lambda region', function (done) {
+		fs.writeFileSync(path.join(workingdir, 'claudia.json'), JSON.stringify({lambda: {name: 'xxx'}}), 'utf8');
+		underTest({source: workingdir}).then(done.fail, function (reason) {
+			expect(reason).toEqual('invalid configuration -- lambda.region missing from claudia.json');
+			done();
+		});
+	});
+	it('updates the lambda with a new version', function (done) {
+		var invokeLambda = Promise.promisify(lambda.invoke.bind(lambda));
+
+		shell.cp('-r', 'spec/test-projects/hello-world/*', workingdir);
+		create({name: testRunName, region: awsRegion, source: workingdir}).then(function (result) {
+			newObjects = { lambdaRole: result.lambda && result.lambda.role, lambdaFunction: result.lambda && result.lambda.name };
+			shell.cp('-rf', 'spec/test-projects/echo/*', workingdir);
+			return underTest({source: workingdir});
+		}).then(function (lambdaFunc) {
+			expect(new RegExp('^arn:aws:lambda:us-east-1:[0-9]+:function:' + testRunName + ':1$').test(lambdaFunc.FunctionArn)).toBeTruthy();
+			expect(lambdaFunc.FunctionName).toEqual(testRunName);
+			return invokeLambda({FunctionName: testRunName, Payload: JSON.stringify({message: 'aloha'})});
+		}).then(function (lambdaResult) {
+			expect(lambdaResult.StatusCode).toEqual(200);
+			expect(lambdaResult.Payload).toEqual('{"message":"aloha"}');
+		}).then(done, done.fail);
+	});
+});
---
+++ src/commands/update.js
@@ -0,0 +1,38 @@
+/*global module, require*/
+var Promise = require('bluebird'),
+	path = require('path'),
+	readjson = require('../util/readjson'),
+	shell = require('shelljs'),
+	zipdir = require('../tasks/zipdir'),
+	collectFiles = require('../tasks/collect-files'),
+	fs = require('fs'),
+	readFile = Promise.promisify(fs.readFile),
+	aws = require('aws-sdk');
+module.exports = function testLambda(options) {
+	'use strict';
+	if (!shell.test('-e', path.join(options.source, 'claudia.json'))) {
+		return Promise.reject('claudia.json does not exist in the source folder');
+	}
+	return readjson(path.join(options.source, 'claudia.json')).then(function (config) {
+		var name = config && config.lambda && config.lambda.name,
+			region = config && config.lambda && config.lambda.region;
+		if (!name) {
+			return Promise.reject('invalid configuration -- lambda.name missing from claudia.json');
+		}
+		if (!region) {
+			return Promise.reject('invalid configuration -- lambda.region missing from claudia.json');
+		}
+		return Promise.resolve(config.lambda);
+	}).then(function (lambdaConfig) {
+		var lambda = new aws.Lambda({region: lambdaConfig.region}),
+			updateLambda = function (fileContents) {
+				var call = Promise.promisify(lambda.updateFunctionCode.bind(lambda));
+				return call({FunctionName: lambdaConfig.name, ZipFile: fileContents, Publish: true});
+			};
+		return collectFiles(options.source).
+				then(zipdir).
+				then(readFile).
+				then(updateLambda);
+	});
+};
+
