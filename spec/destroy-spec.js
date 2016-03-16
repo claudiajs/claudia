64bf5115c9240a2d03e5c142445a67a3a7206897
Marcus Hammarberg <marcusoft.net@gmail.com>
2016-03-16 09:06:53 +0100

    Passing tests for destroying api

--- spec/destroy-spec.js
+++ spec/destroy-spec.js
@@ -1,149 +1,126 @@
 /*global describe, require, it, expect, beforeEach, afterEach, console, jasmine */
 var underTest = require('../src/commands/destroy'),
-	create = require('../src/commands/create'),
-	shell = require('shelljs'),
-	tmppath = require('../src/util/tmppath'),
-	fs = require('fs'),
-	path = require('path'),
-	aws = require('aws-sdk'),
-	Promise = require('bluebird'),
-	awsRegion = 'us-east-1';
-describe('destroy', function () {
-	'use strict';
-	var workingdir, testRunName,  lambda, newObjects;
-	beforeEach(function () {
-		workingdir = tmppath();
-		testRunName = 'test' + Date.now();
-		lambda = Promise.promisifyAll(new aws.Lambda({region: awsRegion}), {suffix: 'Promise'});
-		jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000;
-		newObjects = {workingdir: workingdir};
-		shell.mkdir(workingdir);
-	});
-	it('fails when the source dir does not contain the project config file', function (done) {
-		underTest({source: workingdir}).then(done.fail, function (reason) {
-			expect(reason).toEqual('claudia.json does not exist in the source folder');
-			done();
-		});
-	});
-	it('fails when the project config file does not contain the lambda name', function (done) {
-		fs.writeFileSync(path.join(workingdir, 'claudia.json'), '{}', 'utf8');
-		underTest({source: workingdir}).then(done.fail, function (reason) {
-			expect(reason).toEqual('invalid configuration -- lambda.name missing from claudia.json');
-			done();
-		});
-	});
-	it('fails when the project config file does not contain the lambda region', function (done) {
-		fs.writeFileSync(path.join(workingdir, 'claudia.json'), JSON.stringify({lambda: {name: 'xxx'}}), 'utf8');
-		underTest({source: workingdir}).then(done.fail, function (reason) {
-			expect(reason).toEqual('invalid configuration -- lambda.region missing from claudia.json');
-			done();
-		});
-	});
-	describe('when the lambda project exists', function () {
-		beforeEach(function (done) {
-			shell.cp('-r', 'spec/test-projects/hello-world/*', workingdir);
-			create({name: testRunName, region: awsRegion, source: workingdir, handler: 'main.handler'}).then(function (result) {
+    create = require('../src/commands/create'),
+    shell = require('shelljs'),
+    tmppath = require('../src/util/tmppath'),
+    fs = require('fs'),
+    path = require('path'),
+    aws = require('aws-sdk'),
+    Promise = require('bluebird'),
+    awsRegion = 'us-east-1';
+describe('destroy', function() {
+    'use strict';
+    var workingdir, testRunName, lambda, config, newObjects, apiGateway;
+    beforeEach(function() {
+        workingdir = tmppath();
+        testRunName = 'test' + Date.now();
+        lambda = Promise.promisifyAll(new aws.Lambda({ region: awsRegion }), { suffix: 'Promise' });
+        apiGateway = Promise.promisifyAll(new aws.APIGateway({ region: awsRegion }));
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000;
+        newObjects = { workingdir: workingdir };
+        shell.mkdir(workingdir);
+        config = { name: testRunName, region: awsRegion, source: workingdir, handler: 'main.handler' };
+    });
+    it('fails when the source dir does not contain the project config file', function(done) {
+        underTest({ source: workingdir }).then(done.fail, function(reason) {
+            expect(reason).toEqual('claudia.json does not exist in the source folder');
+            done();
+        });
+    });
+    it('fails when the project config file does not contain the lambda name', function(done) {
+        fs.writeFileSync(path.join(workingdir, 'claudia.json'), '{}', 'utf8');
+        underTest({ source: workingdir }).then(done.fail, function(reason) {
+            expect(reason).toEqual('invalid configuration -- lambda.name missing from claudia.json');
+            done();
+        });
+    });
+    it('fails when the project config file does not contain the lambda region', function(done) {
+        fs.writeFileSync(path.join(workingdir, 'claudia.json'), JSON.stringify({ lambda: { name: 'xxx' } }), 'utf8');
+        underTest({ source: workingdir }).then(done.fail, function(reason) {
+            expect(reason).toEqual('invalid configuration -- lambda.region missing from claudia.json');
+            done();
+        });
+    });
+    describe('when only a lambda function exists', function() {
+        beforeEach(function(done) {
+            shell.cp('-r', 'spec/test-projects/hello-world/*', workingdir);
+            create({ name: testRunName, region: awsRegion, source: workingdir, handler: 'main.handler' }).then(function(result) {
+                shell.cp('-rf', 'spec/test-projects/hello-world/*', workingdir);
+            }).then(done, done.fail);
+        });
+        it('destroys the lambda function', function(done) {
+            underTest({ source: workingdir }).then(function() {
+                return lambda.listVersionsByFunctionPromise({ FunctionName: testRunName });
+            }).catch(function(expectedException) {
+                expect(expectedException.message).toContain(testRunName);
+            })
+            .then(done, done.fail);
+        });
+    });
+    describe('when the lambda project contains a web api', function() {
+        beforeEach(function(done) {
+            shell.cp('-r', 'spec/test-projects/api-gw-hello-world/*', workingdir);
+            create({name: testRunName, region: awsRegion, source: workingdir, 'api-module': 'main'}).then(function (result) {
 				newObjects.lambdaRole = result.lambda && result.lambda.role;
 				newObjects.lambdaFunction = result.lambda && result.lambda.name;
-				shell.cp('-rf', 'spec/test-projects/echo/*', workingdir);
+				newObjects.restApi = result.api && result.api.id;
+                shell.cp('-rf', 'spec/test-projects/api-gw-hello-world/*', workingdir);
 			}).then(done, done.fail);
-		});
-		it('destroys the lambda function', function (done) {
-			underTest({source: workingdir}).then(function () {
-			 	return lambda.listVersionsByFunctionPromise({FunctionName: testRunName});
-			}).catch(function (expectedException) {
-				expect(expectedException.message).toContain(testRunName);
-			})
-			// }).then(function (result) {
-			// 	expect(result.Versions.length).toEqual(0);
-			.then(done, done.fail);
-		});
-		// it('updates the lambda with a new version', function (done) {
-		// 	underTest({source: workingdir}).then(function () {
-		// 		return lambda.invokePromise({FunctionName: testRunName, Payload: JSON.stringify({message: 'aloha'})});
-		// 	}).then(function (lambdaResult) {
-		// 		expect(lambdaResult.StatusCode).toEqual(200);
-		// 		expect(lambdaResult.Payload).toEqual('{"message":"aloha"}');
-		// 	}).then(done, done.fail);
-		// });
-		// it('adds the version alias if supplied', function (done) {
-		// 	underTest({source: workingdir, version: 'great'}).then(function () {
-		// 		return lambda.getAliasPromise({FunctionName: testRunName, Name: 'great'});
-		// 	}).then(function (result) {
-		// 		expect(result.FunctionVersion).toEqual('2');
-		// 	}).then(done, done.fail);
-		// });
-
-		// it('checks the current dir if the source is not provided', function (done) {
-		// 	shell.cd(workingdir);
-		// 	underTest().then(function (lambdaFunc) {
-		// 		expect(new RegExp('^arn:aws:lambda:us-east-1:[0-9]+:function:' + testRunName + ':2$').test(lambdaFunc.FunctionArn)).toBeTruthy();
-		// 		expect(lambdaFunc.FunctionName).toEqual(testRunName);
-		// 		return lambda.invokePromise({FunctionName: testRunName, Payload: JSON.stringify({message: 'aloha'})});
-		// 	}).then(done, done.fail);
-		// });
-	});
-	// describe('when the lambda project contains a web api', function () {
-	// 	var originaldir, updateddir;
-	// 	beforeEach(function (done) {
-	// 		originaldir =  path.join(workingdir, 'original');
-	// 		updateddir = path.join(workingdir, 'updated');
-	// 		shell.mkdir(originaldir);
-	// 		shell.mkdir(updateddir);
-	// 		shell.cp('-r', 'spec/test-projects/api-gw-hello-world/*', originaldir);
-	// 		shell.cp('-r', 'spec/test-projects/api-gw-echo/*', updateddir);
-	// 		create({name: testRunName, version: 'original', region: awsRegion, source: originaldir, 'api-module': 'main'}).then(function (result) {
-	// 			newObjects.lambdaRole = result.lambda && result.lambda.role;
-	// 			newObjects.lambdaFunction = result.lambda && result.lambda.name;
-	// 			newObjects.restApi = result.api && result.api.id;
-	// 			shell.cp(path.join(originaldir, 'claudia.json'), updateddir);
-	// 		}).then(done, done.fail);
-	// 	});
-	// 	it('updates the api using the configuration from the api module', function (done) {
-	// 		return underTest({source: updateddir}).then(function () {
-	// 			return invoke('latest/echo?name=mike');
-	// 		}).then(function (contents) {
-	// 			var params = JSON.parse(contents.body);
-	// 			expect(params.queryString).toEqual({name: 'mike'});
-	// 			expect(params.context.method).toEqual('GET');
-	// 			expect(params.context.path).toEqual('/echo');
-	// 			expect(params.env).toEqual({
-	// 				lambdaVersion: 'latest'
-	// 			});
-	// 		}).then(done, done.fail);
-	// 	});
-	// 	it('when the version is provided, creates the deployment with that name', function (done) {
-	// 		underTest({source: updateddir, version: 'development'}).then(function () {
-	// 			return invoke('development/echo?name=mike');
-	// 		}).then(function (contents) {
-	// 			var params = JSON.parse(contents.body);
-	// 			expect(params.queryString).toEqual({name: 'mike'});
-	// 			expect(params.context.method).toEqual('GET');
-	// 			expect(params.context.path).toEqual('/echo');
-	// 			expect(params.env).toEqual({
-	// 				lambdaVersion: 'development'
-	// 			});
-	// 		}).then(done, done.fail);
-	// 	});
-	// 	it('if using a different version, leaves the old one intact', function (done) {
-	// 		underTest({source: updateddir, version: 'development'}).then(function () {
-	// 			return invoke('original/hello');
-	// 		}).then(function (contents) {
-	// 			expect(contents.body).toEqual('"hello world"');
-	// 		}).then(done, done.fail);
-	// 	});
-	// 	it('if using the same version, rewrites the old one', function (done) {
-	// 		underTest({source: updateddir, version: 'original'}).then(function () {
-	// 			return invoke('original/echo?name=mike');
-	// 		}).then(function (contents) {
-	// 			var params = JSON.parse(contents.body);
-	// 			expect(params.queryString).toEqual({name: 'mike'});
-	// 			expect(params.context.method).toEqual('GET');
-	// 			expect(params.context.path).toEqual('/echo');
-	// 			expect(params.env).toEqual({
-	// 				lambdaVersion: 'original'
-	// 			});
-	// 		}).then(done, done.fail);
-	// 	});
-	// });
+        });
+        it('destroys the web api function', function(done) {
+            underTest({ source: workingdir }).then(function() {
+                return apiGateway.getRestApi({ restApiId: newObjects.restApi });
+            }).catch(function(expectedException) {
+                expect(expectedException.message).toContain(newObjects.restApi);
+            })
+            .then(done, done.fail);
+        });
+    });
+    // 	it('updates the api using the configuration from the api module', function (done) {
+    // 		return underTest({source: updateddir}).then(function () {
+    // 			return invoke('latest/echo?name=mike');
+    // 		}).then(function (contents) {
+    // 			var params = JSON.parse(contents.body);
+    // 			expect(params.queryString).toEqual({name: 'mike'});
+    // 			expect(params.context.method).toEqual('GET');
+    // 			expect(params.context.path).toEqual('/echo');
+    // 			expect(params.env).toEqual({
+    // 				lambdaVersion: 'latest'
+    // 			});
+    // 		}).then(done, done.fail);
+    // 	});
+    // 	it('when the version is provided, creates the deployment with that name', function (done) {
+    // 		underTest({source: updateddir, version: 'development'}).then(function () {
+    // 			return invoke('development/echo?name=mike');
+    // 		}).then(function (contents) {
+    // 			var params = JSON.parse(contents.body);
+    // 			expect(params.queryString).toEqual({name: 'mike'});
+    // 			expect(params.context.method).toEqual('GET');
+    // 			expect(params.context.path).toEqual('/echo');
+    // 			expect(params.env).toEqual({
+    // 				lambdaVersion: 'development'
+    // 			});
+    // 		}).then(done, done.fail);
+    // 	});
+    // 	it('if using a different version, leaves the old one intact', function (done) {
+    // 		underTest({source: updateddir, version: 'development'}).then(function () {
+    // 			return invoke('original/hello');
+    // 		}).then(function (contents) {
+    // 			expect(contents.body).toEqual('"hello world"');
+    // 		}).then(done, done.fail);
+    // 	});
+    // 	it('if using the same version, rewrites the old one', function (done) {
+    // 		underTest({source: updateddir, version: 'original'}).then(function () {
+    // 			return invoke('original/echo?name=mike');
+    // 		}).then(function (contents) {
+    // 			var params = JSON.parse(contents.body);
+    // 			expect(params.queryString).toEqual({name: 'mike'});
+    // 			expect(params.context.method).toEqual('GET');
+    // 			expect(params.context.path).toEqual('/echo');
+    // 			expect(params.env).toEqual({
+    // 				lambdaVersion: 'original'
+    // 			});
+    // 		}).then(done, done.fail);
+    // 	});
+    // });
 });
--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -2,7 +2,7 @@
 var Promise = require('bluebird'),
     aws = require('aws-sdk'),
     loadConfig = require('../util/loadconfig');
-module.exports = function update(options) {
+module.exports = function destroy(options) {
     'use strict';
     var lambda, lambdaConfig, apiConfig, deleteFunction,
         iam = Promise.promisifyAll(new aws.IAM()),
@@ -22,24 +22,23 @@ module.exports = function update(options) {
 
     return loadConfig(options, { lambda: { name: true, region: true, role: true } }).then(function(config) {
         lambdaConfig = config.lambda;
-    })
-    // .then(function() {
-    //     var apiGateway = Promise.promisifyAll(new aws.APIGateway({ region: lambdaConfig.region }));
-    //     console.log(apiGateway);
-    //     console.log("######");
-    //     console.log(lambdaConfig.name);
-    //     return apiGateway.deleteRestApiAsync({
-    //         restApiId: lambdaConfig.name
-    //     });
-    // });
-    .then(function() {
+        apiConfig = config.api;
+        console.log(config);
+    }).then(function() {
         lambda = Promise.promisifyAll(new aws.Lambda({ region: lambdaConfig.region }), { suffix: 'Promise' });
         deleteFunction = Promise.promisify(lambda.deleteFunction.bind(lambda));
         return deleteFunction({ FunctionName: lambdaConfig.name });
+    }).then(function() {
+        if(!apiConfig){
+            return Promise.resolve(true);
+        }
+        var apiGateway = Promise.promisifyAll(new aws.APIGateway({ region: lambdaConfig.region }));
+        return apiGateway.deleteRestApiAsync({
+            restApiId: apiConfig.id
+        });
+    }).then(function() {
+        if (lambdaConfig.role) {
+            return destroyRole(lambdaConfig.role);
+        }
     });
-    // }).then(function() {
-    //     if (newObjects.lambdaRole) {
-    //         return destroyRole(newObjects.lambdaRole);
-    //     }
-    // });
 };
