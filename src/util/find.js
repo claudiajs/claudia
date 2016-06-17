9a79b8da737f894c34d5620f013445eafcab8239
Ante Gulin <theguxi@gmail.com>
2016-06-17 23:51:45 +0200

    Performance improvement find.js
    Removed explicit overhead/iterations in the find function.

--- src/util/find.js
+++ src/util/find.js
@@ -1,12 +1,11 @@
 /*global module */
 module.exports = function find(array, predicate, context) { /* no .find support in 10.0 */
 	'use strict';
-	var result;
 	array.forEach(function (element) {
 		if (!result && predicate(element, context)) {
-			result = element;
+			return element;
 		}
 	});
-	return result;
+	return undefined;
 };
 
