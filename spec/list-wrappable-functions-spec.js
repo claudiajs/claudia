f45ab4ed2b24805b566b4f3fd812f709fbbde6e0
Gojko Adzic <gojko@gojko.com>
2016-11-16 08:41:22 +0100

    better handling of supertypes

--- spec/list-wrappable-functions-spec.js
+++ spec/list-wrappable-functions-spec.js
@@ -1,7 +1,8 @@
 /*global describe, expect, it, require */
 var aws = require('aws-sdk'),
 	listWrappableFunctions = require('../src/util/list-wrappable-functions'),
-	iam = new aws.IAM();
+	iam = new aws.IAM(),
+	s3 = new aws.S3();
 describe('listWrappableFunctions', function () {
 	'use strict';
 	it('should identify methods', function () {
@@ -9,6 +10,12 @@ describe('listWrappableFunctions', function () {
 	});
 	it('should ignore generic methods', function () {
 		expect(listWrappableFunctions(iam)).not.toContain('makeRequest');
+	});
+	it('should ignore constructors', function () {
+		expect(listWrappableFunctions(iam)).not.toContain('constructor');
+	});
+	it('should include super-prototype methods', function () {
+		expect(listWrappableFunctions(s3)).toContain('upload');
 	});
 	it('should not contain any properties', function () {
 		expect(listWrappableFunctions({ a: function () { }, b: 5})).toEqual(['a']);
