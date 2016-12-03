8d4ddd7701c319a650efbba076fdff47232881f3
Gojko Adzic <gojko@gojko.com>
2016-12-03 22:57:54 +0100

    ci setup to log without colors

--- spec/support/jasmine-runner.js
+++ spec/support/jasmine-runner.js
@@ -1,14 +1,29 @@
 /*global jasmine, require, process*/
 var Jasmine = require('jasmine'),
 	SpecReporter = require('jasmine-spec-reporter'),
-	noop = function () {},
 	jrunner = new Jasmine(),
 	filter;
 process.argv.slice(2).forEach(function (option) {
 	'use strict';
 	if (option === 'full') {
-		jrunner.configureDefaultReporter({print: noop});    // remove default reporter logs
-		jasmine.getEnv().addReporter(new SpecReporter());   // add jasmine-spec-reporter
+		jasmine.getEnv().clearReporters();
+		jasmine.getEnv().addReporter(new SpecReporter({
+			displayStacktrace: 'all'
+		}));
+	}
+	if (option === 'ci') {
+		jasmine.getEnv().clearReporters();
+		jasmine.getEnv().addReporter(new SpecReporter({
+			displayStacktrace: 'all',
+			displaySpecDuration: true,
+			displaySuiteNumber: true,
+			colors: false,
+			prefixes: {
+				success: '[pass] ',
+				failure: '[fail] ',
+				pending: '[skip] '
+			}
+		}));
 	}
 	if (option.match('^filter=')) {
 		filter = option.match('^filter=(.*)')[1];
