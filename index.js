b975fbd0e81fde94da8b900873e09482fc218a8b
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2017-01-04 20:51:32 +0100

    Convert index.js file to es6

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /* global __dirname, require, module */
-var shell = require('shelljs'),
+const shell = require('shelljs'),
 	path = require('path'),
 	readCommands = function () {
 		'use strict';
-		var result = {};
-		shell.ls(path.join(__dirname, './src/commands')).forEach(function (fileName) {
-			var cmdName = path.basename(fileName, '.js'),
-				cmdFunc = require('./src/commands/' + cmdName);
+		const result = {};
+		shell.ls(path.join(__dirname, './src/commands')).forEach(fileName => {
+			const cmdName = path.basename(fileName, '.js'),
+				cmdFunc = require(`./src/commands/${cmdName}`);
 			result[cmdFunc.name] = cmdFunc;
 		});
 		return result;
 	};
 module.exports = readCommands();
-
