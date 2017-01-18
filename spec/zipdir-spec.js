56223d5de0ad8846a519b5898e5f0d9a1f1e53a1
Gojko Adzic <gojko@gojko.com>
2017-01-18 15:57:04 +0000

    linting

--- spec/zipdir-spec.js
+++ spec/zipdir-spec.js
@@ -1,5 +1,5 @@
 /*global describe, it, expect, beforeEach, afterEach, require */
-var shell = require('shelljs'),
+const shell = require('shelljs'),
 	fs = require('fs'),
 	os = require('os'),
 	path = require('path'),
@@ -7,46 +7,46 @@ var shell = require('shelljs'),
 	trimSlash = require('../src/util/trimslash'),
 	underTest = require('../src/tasks/zipdir');
 
-describe('zipdir', function () {
+describe('zipdir', () => {
 	'use strict';
-	var workingdir, zipfile, pwd;
-	beforeEach(function () {
+	let workingdir, zipfile, pwd;
+	beforeEach(() => {
 		workingdir = tmppath();
 		shell.mkdir(workingdir);
 		pwd = shell.pwd();
 		zipfile = false;
 	});
-	afterEach(function () {
+	afterEach(() => {
 		shell.cd(pwd);
 		shell.rm('-rf', workingdir);
 		if (zipfile) {
 			shell.rm('-rf', zipfile);
 		}
 	});
-	it('rejects if the path does not exist', function (done) {
-		var argpath = tmppath();
-		underTest(argpath).then(done.fail, function (reason) {
+	it('rejects if the path does not exist', done => {
+		const argpath = tmppath();
+		underTest(argpath).then(done.fail, reason => {
 			expect(reason).toEqual(argpath + ' does not exist');
 			done();
 		});
 	});
-	it('rejects if the path is not a dir', function (done) {
-		var filePath = path.join(workingdir, 'root.txt');
+	it('rejects if the path is not a dir', done => {
+		const filePath = path.join(workingdir, 'root.txt');
 		fs.writeFileSync(filePath, 'text1', 'utf8');
-		underTest(filePath).then(done.fail, function (reason) {
+		underTest(filePath).then(done.fail, (reason) => {
 			expect(reason).toEqual(filePath + ' is not a directory');
 			done();
 		});
 	});
-	it('zips up files and subfolders into a temporary path', function (done) {
-		var original = path.join(workingdir, 'original');
+	it('zips up files and subfolders into a temporary path', done => {
+		const original = path.join(workingdir, 'original');
 		shell.mkdir(original);
 		fs.writeFileSync(path.join(original, 'root.txt'), 'text1', 'utf8');
 		shell.mkdir(path.join(original, 'subdir'));
 		fs.writeFileSync(path.join(original, 'subdir', 'sub.txt'), 'text2', 'utf8');
 
-		underTest(original).then(function (argpath) {
-			var unpacked = path.join(workingdir, 'unpacked');
+		underTest(original).then(argpath => {
+			const unpacked = path.join(workingdir, 'unpacked');
 
 			zipfile = argpath;
 			shell.mkdir(unpacked);
@@ -60,12 +60,12 @@ describe('zipdir', function () {
 			expect(fs.readFileSync(path.join(unpacked, 'subdir', 'sub.txt'), 'utf8')).toEqual('text2');
 		}).then(done, done.fail);
 	});
-	it('removes the original dir if successful', function (done) {
-		var original = path.join(workingdir, 'original');
+	it('removes the original dir if successful', done => {
+		const original = path.join(workingdir, 'original');
 
 		shell.mkdir(original);
 		fs.writeFileSync(path.join(original, 'root.txt'), 'text1', 'utf8');
-		underTest(original).then(function () {
+		underTest(original).then(() => {
 			expect(shell.test('-e', original)).toBeFalsy();
 		}).then(done, done.fail);
 	});
