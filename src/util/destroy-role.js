f1124d35883d34987367a0854b98c87a5c69f96e
Gojko Adzic <gojko@gojko.com>
2017-02-17 19:25:19 +0100

    fixes https://github.com/claudiajs/claudia/issues/100

--- src/util/destroy-role.js
+++ src/util/destroy-role.js
@@ -1,13 +1,13 @@
-const aws = require('aws-sdk'),
-	iam = new aws.IAM();
+const aws = require('aws-sdk');
 module.exports = function destroyRole(roleName) {
 	'use strict';
-	const deleteSinglePolicy = function (policyName) {
-		return iam.deleteRolePolicy({
-			PolicyName: policyName,
-			RoleName: roleName
-		}).promise();
-	};
+	const iam = new aws.IAM(),
+		deleteSinglePolicy = function (policyName) {
+			return iam.deleteRolePolicy({
+				PolicyName: policyName,
+				RoleName: roleName
+			}).promise();
+		};
 	return iam.listRolePolicies({RoleName: roleName}).promise()
 	.then(result => Promise.all(result.PolicyNames.map(deleteSinglePolicy)))
 	.then(() => iam.deleteRole({RoleName: roleName}).promise());
