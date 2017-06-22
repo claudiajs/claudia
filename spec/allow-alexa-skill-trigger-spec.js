7d4ed4e0e0bbb80d5e2d07020751cb15622dadac
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2017-06-22 19:27:50 +0200

    Allow Alexa Skill to trigger lambda

---
+++ spec/allow-alexa-skill-trigger-spec.js
@@ -0,0 +1,45 @@
+/*global describe, it, expect, beforeEach, afterEach */
+const underTest = require('../src/commands/allow-alexa-skill-trigger'),
+	create = require('../src/commands/create'),
+	destroyObjects = require('./util/destroy-objects'),
+	shell = require('shelljs'),
+	tmppath = require('../src/util/tmppath'),
+	aws = require('aws-sdk'),
+	awsRegion = require('./util/test-aws-region');
+
+describe('allowAlexaSkillTrigger', () => {
+	'use strict';
+
+	let workingdir, testRunName, newObjects, lambda;
+	beforeEach(() => {
+		workingdir = tmppath();
+		lambda = new aws.Lambda({ region: awsRegion });
+		testRunName = 'test' + Date.now();
+		newObjects = { workingdir: workingdir };
+		shell.mkdir(workingdir);
+	});
+	afterEach(done => {
+		destroyObjects(newObjects).then(done, done.fail);
+	});
+	it('allows Alexa Skill to trigger Lambda', done => {
+		const createConfig = { name: testRunName, region: awsRegion, source: workingdir, handler: 'main.handler', version: 'dev' }, config = {
+				source: workingdir,
+				version: 'dev'
+			};
+		shell.cp('-r', 'spec/test-projects/hello-world/*', workingdir);
+		create(createConfig)
+      .then(result => {
+	newObjects.lambdaRole = result.lambda && result.lambda.role;
+	newObjects.lambdaFunction = result.lambda && result.lambda.name;
+})
+      .then(() => underTest(config))
+      .then(() => lambda.getPolicy({ FunctionName: testRunName, Qualifier: 'dev' }).promise())
+      .then(result => JSON.parse(result.Policy).Statement[0])
+      .then(statement => {
+	expect(statement.Effect).toEqual('Allow');
+	expect(statement.Principal.Service).toEqual('alexa-appkit.amazon.com');
+	expect(statement.Action).toEqual('lambda:InvokeFunction');
+})
+      .then(done, done.fail);
+	});
+});
---
+++ src/commands/allow-alexa-skill-trigger.js
@@ -0,0 +1,58 @@
+const loadConfig = require('../util/loadconfig'),
+	aws = require('aws-sdk');
+
+module.exports = function allowAlexaSkillTrigger(options) {
+	'use strict';
+	let lambdaConfig,
+		lambda;
+	const initServices = function () {
+			lambda = new aws.Lambda({region: lambdaConfig.region});
+		},
+		getLambda = () => lambda.getFunctionConfiguration({FunctionName: lambdaConfig.name, Qualifier: options.version}).promise(),
+		readConfig = function () {
+			return loadConfig(options, {lambda: {name: true, region: true}})
+				.then(config => {
+					lambdaConfig = config.lambda;
+				})
+				.then(initServices)
+				.then(getLambda)
+				.then(result => result.Version);
+		},
+		addInvokePermission = function (version) {
+			return lambda.addPermission({
+				Action: 'lambda:InvokeFunction',
+				FunctionName: lambdaConfig.name,
+				Principal: 'alexa-appkit.amazon.com',
+				Qualifier: options.version || version,
+				StatementId: `Alexa-${Date.now()}`
+			}).promise();
+		};
+	return readConfig()
+		.then(addInvokePermission)
+    .then(result => JSON.parse(result.Statement));
+};
+module.exports.doc = {
+	description: 'Allow Alexa Skill triggers',
+	priority: 5,
+	args: [
+		{
+			argument: 'version',
+			optional: true,
+			description: 'Bind to a particular version',
+			example: 'production',
+			default: 'latest version'
+		},
+		{
+			argument: 'source',
+			optional: true,
+			description: 'Directory with project files',
+			default: 'current directory'
+		},
+		{
+			argument: 'config',
+			optional: true,
+			description: 'Config file containing the resource names',
+			default: 'claudia.json'
+		}
+	]
+};
