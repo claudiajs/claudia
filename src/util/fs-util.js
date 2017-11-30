305ea1050ab263fe9ce7d23b3682f81654914bc7
simalexan <alexander.simovic@gmail.com>
2017-11-30 18:59:54 +0100

    removing two unneeded methods for fs-util

--- src/util/fs-util.js
+++ src/util/fs-util.js
@@ -9,10 +9,6 @@ exports.rmDir = function (dirPath) {
 	'use strict';
 	shell.rm('-rf', dirPath);
 };
-exports.renameFile = function (currentFilePath, newFilePath) {
-	'use strict';
-	shell.mv(currentFilePath, newFilePath);
-};
 exports.fileExists = function (filePath) {
 	'use strict';
 	return shell.test('-e', filePath);
@@ -33,11 +29,6 @@ exports.copy = function (from, to) {
 exports.recursiveList = function (dirPath) {
 	'use strict';
 	return shell.ls('-R', dirPath);
-};
-exports.makeDir = function (dirPath) {
-	'use strict';
-	shell.mkdir('-p', dirPath);
-	return Promise.resolve();
 };
 exports.replaceStringInFile = function (searchPattern, replacePattern, filePath) {
 	'use strict';
